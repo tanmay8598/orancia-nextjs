5189773161fff1221ac2b58b8be00177271f6b2f
vimal_zonvoir <vimal@zonvoir.com>
2024-05-22 15:58:31 +0530

    dynamic navbar

--- src/components/Navbar/MenuNavbar.js
+++ src/components/Navbar/MenuNavbar.js
@@ -15,6 +15,7 @@ import useAuth from "@/auth/useAuth";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import MenuSidebar from "../Cart/MenuSidebar";
+import MainmenuNavbar from "./MainmenuNavbar";
 
 const MenuNavbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -36,45 +37,6 @@ const MenuNavbar = () => {
   return (
     <>
       <div className="shadow-md sticky top-0 z-50 bg-white ">
-        {/* Mobile menu */}
-        {/* <Transition.Root show={open} as={Fragment}>
-          <Dialog className="relative z-40 lg:hidden" onClose={setOpen}>
-            <Transition.Child
-              as={Fragment}
-              enter="transition-opacity ease-linear duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-opacity ease-linear duration-300"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <div className="fixed inset-0 bg-black bg-opacity-25" />
-            </Transition.Child>
-
-            <div className="fixed inset-0 z-40 flex">
-              <Transition.Child
-                as={Fragment}
-                enter="transition ease-in-out duration-300 transform"
-                enterFrom="-translate-x-full"
-                enterTo="translate-x-0"
-                leave="transition ease-in-out duration-300 transform"
-                leaveFrom="translate-x-0"
-                leaveTo="-translate-x-full"
-              >
-                <Dialog.Panel className="relative flex w-full mt-24 max-w-xs flex-col overflow-y-auto bg-white pb-12 shadow-xl">
-                  
-
-                  <div className="space-y-6 border-t pt-9 border-gray-200 px-4 py-6">
-                    <MobNavbarItems />
-                  </div>
-
-                  <div className="space-y-6 border-t border-gray-200 px-4 py-6"></div>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </Dialog>
-        </Transition.Root> */}
-
         <header className="relative bg-white">
           <section className="py-2 bg-primary text-center md-py-3">
             <p className="text-white text-xs md:text-sm">
@@ -111,7 +73,8 @@ const MenuNavbar = () => {
                   <div className="flex h-full space-x-8">
                     <div className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800">
                       <div className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800">
-                        <NavbarItems />
+                        {/* <NavbarItems /> */}
+                        <MainmenuNavbar />
                       </div>
                     </div>
                   </div>
