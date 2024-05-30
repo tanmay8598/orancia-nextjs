5228efd073c601f7a68bc85e99467263a30ac00b
vimal_zonvoir <vimal@zonvoir.com>
2024-05-30 13:26:03 +0530

    bestsaller carousel

--- src/components/BestSellers/BestSellerCarousel.js
+++ src/components/BestSellers/BestSellerCarousel.js
@@ -1,93 +1,113 @@
 "use client";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-
 import "swiper/css";
 import "swiper/css/pagination";
-
 import { Pagination } from "swiper/modules";
-import Product from "./Product";
-import { useCallback, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import BestSallerCard from "./BestSallerCard";
-
+import "./style.css";
+import Slider from "react-slick";
 const BestSellerCarousel = ({ products }) => {
+  const [isMobileView, setIsMobileView] = useState(false);
   const sliderRef = useRef(null);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileView(window.innerWidth < 768);
+    };
 
-  const handlePrev = useCallback(() => {
-    if (!sliderRef.current) return;
-    sliderRef.current.swiper.slidePrev();
+    handleResize(); // Check initial screen size
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
-
-  const handleNext = useCallback(() => {
-    if (!sliderRef.current) return;
-    sliderRef.current.swiper.slideNext();
-  }, []);
-
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1.25,
+    slidesToScroll: 1.25,
+  };
   return (
-    <div className="flex items-center justify-cente">
-      <button
-        onClick={handlePrev}
-        className="hover:drop-shadow-md hover:bg-white hover:p-2 hover:rounded-lg"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
-          />
-        </svg>
-      </button>
+    <>
+      {isMobileView ? (
+        <>
+          <Slider {...settings}>
+            {products.map((product) => {
+              return <BestSallerCard product={product} />;
+            })}
+          </Slider>
+        </>
+      ) : (
+        <div className="flex items-center justify-cente">
+          <button
+            // onClick={handlePrev}
+            onClick={() => sliderRef.current.swiper.slidePrev()}
+            className="hover:drop-shadow-md hover:bg-white hover:p-2 hover:rounded-lg"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+              />
+            </svg>
+          </button>
 
-      <Swiper
-        ref={sliderRef}
-        slidesPerView={1}
-        spaceBetween={30}
-        breakpoints={{
-          640: { slidesPerView: 1 },
-          768: { slidesPerView: 2 },
-          960: { slidesPerView: 4 },
-          1440: { slidesPerView: 4 },
-        }}
-        modules={[Pagination]}
-        className="popular-bike-slider mb-8 "
-      >
-        {products.map((product) => {
-          return (
-            <SwiperSlide key={product._id}>
-              {/* <Product product={product} /> */}
-              {/* <NewProducts product={product} /> */}
-              <BestSallerCard product={product} />
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
-      <button
-        onClick={handleNext}
-        className="hover:drop-shadow-md hover:bg-white hover:p-2 hover:rounded-lg"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
-          />
-        </svg>
-      </button>
-    </div>
+          <Swiper
+            ref={sliderRef}
+            slidesPerView={1}
+            spaceBetween={30}
+            breakpoints={{
+              640: { slidesPerView: 1 },
+              768: { slidesPerView: 2 },
+              960: { slidesPerView: 4 },
+              1440: { slidesPerView: 4 },
+            }}
+            modules={[Pagination]}
+            className="popular-bike-slider mb-8 "
+          >
+            {products.map((product) => {
+              return (
+                <SwiperSlide key={product._id}>
+                  {/* <Product product={product} /> */}
+                  {/* <NewProducts product={product} /> */}
+                  <BestSallerCard product={product} />
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+          <button
+            // onClick={handleNext}
+
+            onClick={() => sliderRef.current.swiper.slideNext()}
+            className="hover:drop-shadow-md hover:bg-white hover:p-2 hover:rounded-lg"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+              />
+            </svg>
+          </button>
+        </div>
+      )}
+    </>
   );
 };
 
